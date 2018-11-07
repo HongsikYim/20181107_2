1fd80440a9cbccf1ad837871edd5890699bb6457
HOIK <yimhongsik@gmail.com>
2018-11-07 15:38:25 +0900

    :)

---
+++ main.c
@@ -0,0 +1,38 @@
+/*
+ * 20181107_practice3_git.c
+ *
+ * Created: 2018-11-07 오후 3:36:33
+ * Author : kccistc
+ */ 
+
+#define F_CPU 16000000L
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	DDRD = 0xff;
+	PORTD = 0x00;
+	
+	while (1)
+	{
+		for(int i = 0 ; i < 2; i++)
+		{
+			for(int j = 0 ; j < 8 ; j++)
+			{
+				if(i == 0)
+				{
+					PORTD = 1 << j;
+				}
+				else if(i == 1)
+				{
+					PORTD = 0x80 >> j;
+				}
+				_delay_ms(300);
+			}
+		}
+	}
+}
